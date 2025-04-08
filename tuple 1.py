3f21b89b35cc72966d657296c2f5503f26ae96a2
Sakshi Rahane <101510514+sakshirahane@users.noreply.github.com>
2025-04-08 18:16:04 +0530

    Delete tuple 1.py

--- tuple 1.py
+++
@@ -1,9 +0,0 @@
-#Write a Python program to find the number of times 4 appears in the tuple.
-#Input: tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7 )
-
-
-tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
-
-count = tuplex.count(4)
-
-print(f"The number 4 appears {count} times in the tuple.")
